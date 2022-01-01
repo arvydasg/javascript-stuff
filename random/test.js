87fef9e097173c7a4118da5b2836ec908023eefe
arvydasg <azegaspa@gmail.com>
2022-01-01 13:23:10 +0200

    more mod

--- random/test.js
+++ random/test.js
@@ -1,8 +1,10 @@
 // sitas - https://www.youtube.com/watch?v=HnXmI2UVZlU
 let my_element = document.createElement('h1'); // sukuriu  h1 tag
 
-my_element.innerText = "your mother"; // noriu, kad H1 tag sakytu tai:
+my_element.innerText = "Labukas"; // noriu, kad H1 tag sakytu tai:
 
 document.body.appendChild(my_element); // render H1 tag
 
 my_element.classList.add('custom_class'); // pridek css klase prie tag
+
+my_element.style.backgroundColor = 'red'
