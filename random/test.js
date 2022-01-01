8424b0eeed364f8338b5dd203baf7756a8452b14
arvydasg <azegaspa@gmail.com>
2022-01-01 12:43:43 +0200

    mm

---
+++ random/test.js
@@ -0,0 +1,8 @@
+// sitas - https://www.youtube.com/watch?v=HnXmI2UVZlU
+let my_element = document.createElement('h1'); // sukuriu  h1 tag
+
+my_element.innerText = "your mother"; // noriu, kad H1 tag sakytu tai:
+
+document.body.appendChild(my_element); // render H1 tag
+
+my_element.classList.add('custom_class'); // pridek css klase prie tag
